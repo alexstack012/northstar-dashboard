bbc08f3ef5ba4acbec06aadd11e8ab7550717a00
Alex Stack <alex_stack012@live.com>
2026-03-03 13:14:07 -0600

    structure update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+// import { LoginComponent } from './components/login/login.component';
+// import { ShellLayoutComponent } from './components/shell-layout/shell-layout.component';
+// import { AuthGuard } from './guards/auth.guard';
+// import { AdminGuard } from './guards/admin.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+//   { path: 'login', component: LoginComponent },
+
+//   {
+//     path: '',
+//     canActivate: [AuthGuard],
+//     component: ShellLayoutComponent,
+//     children: [
+//       { path: 'dashboard', loadChildren: () => import('./features/dashboard/routes') },
+//       { path: 'jobs', loadChildren: () => import('./features/jobs/routes') },
+//       { path: 'candidates', loadChildren: () => import('./features/candidates/routes') },
+//       { path: 'users', canActivate: [AdminGuard], loadChildren: () => import('./features/users/routes') }
+//     ]
+//   },
+
+//   { path: '**', redirectTo: 'dashboard' }
+];
